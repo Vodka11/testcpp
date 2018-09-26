9beac2d19e192692cd994e9036bd3013eacd1556
Adrien <adrienheffer@yahoo.fr>
2018-09-26 10:31:06 +0200

    library

---
+++ src/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include "hello.h"
+
+using namespace std;
+
+void h::hello(){
+	cout<<"Hello World"<<endl;
+}
+void h::salut(){
+	cout<<"Salut"<<endl;
+}
+void h::tbo(){
+	cout<<"Hey Boy"<<endl;
+}
+
---
+++ src/hello.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class h
+{
+public :
+void hello();
+void salut();
+void tbo();
+
+};
